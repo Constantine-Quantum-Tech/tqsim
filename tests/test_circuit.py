7f6bcfca61be06b8f3da461c467e7efa7c6ea7fc
Abduhu <abdellah.a.tounsi@gmail.com>
2025-11-03 13:46:23 +0100

    [tests] define custom model

--- tests/test_circuit.py
+++ tests/test_circuit.py
@@ -12,9 +12,11 @@
 
 import os
 import pytest
+import itertools
 import numpy as np
 
 from tqsim import AnyonicCircuit
+from tqsim.lib.anyon_model import AnyonModel
 from tqsim.models.ising import ISING_MODEL
 from tqsim.models.fibonacci import FIBONACCI_MODEL
 
@@ -32,7 +34,7 @@ def test_init_1():
 
     assert circuit
     assert circuit.nb_qudits == 1
-    assert circuit.nb_anyons_per_qudits == 3
+    assert circuit.nb_anyons_per_qudit == 3
     assert circuit.dim == 3
     assert circuit.braiding_operators
     assert circuit.basis
@@ -45,7 +47,7 @@ def test_init_2():
 
     assert circuit
     assert circuit.nb_qudits == 1
-    assert circuit.nb_anyons_per_qudits == 4
+    assert circuit.nb_anyons_per_qudit == 4
     assert circuit.dim == 5
 
 
@@ -54,20 +56,57 @@ def test_init_3():
 
     assert circuit
     assert circuit.nb_qudits == 2
-    assert circuit.nb_anyons_per_qudits == 4
+    assert circuit.nb_anyons_per_qudit == 4
     assert circuit.dim == 34
 
 
-def test_init_model():
+def test_init_model_1():
+    # Z_N model (Abelian model)
+    N = 5
+    N_symbols = np.zeros((N, N, N), dtype=int)
+    for i, j, k in itertools.product(range(N), repeat=3):
+        if (i + j) % N == k:
+            N_symbols[i, j, k] = 1
+
+    F_matrix = np.zeros((N, N, N, N, N, N), dtype=complex)
+    for i, j, k, l, m, n in itertools.product(range(N), repeat=6):
+        if (i + j + k) % N == l and (i + j) % N == m and (j + k) % N == n:
+            F_matrix[i, j, k, l, m, n] = 1
+
+    R_matrix = np.zeros((N, N, N), dtype=complex)
+    for i, j, k in itertools.product(range(N), repeat=3):
+        if (i + j) % N == k:
+            R_matrix[i, j, k] = np.exp(2j * np.pi * i * j / N)
+
+    zn_model = AnyonModel(N_symbols, F_matrix, R_matrix, name="Z_N")
+
     circuit = AnyonicCircuit(
-        nb_qudits=1,
-        nb_anyons_per_qudit=3,
-        model=ISING_MODEL,
-        input_charge=1
-        )
+        nb_qudits=1, nb_anyons_per_qudit=4, model=zn_model, input_charge=1
+    )
+
     assert circuit
     assert circuit.nb_qudits == 1
-    assert circuit.nb_anyons_per_qudits == 3
+    assert circuit.nb_anyons_per_qudit == 4
+    assert circuit.input_charge == 1
+    assert circuit.model.name == "Z_N"
+    assert circuit.dim == 1
+    assert len(circuit.braiding_operators) == 3
+    assert circuit.braiding_operators[0].shape == (1, 1)
+
+    try:
+        circuit.model = FIBONACCI_MODEL
+        assert False
+    except AttributeError:
+        assert True
+
+
+def test_init_model_2():
+    circuit = AnyonicCircuit(
+        nb_qudits=1, nb_anyons_per_qudit=3, model=ISING_MODEL, input_charge=1
+    )
+    assert circuit
+    assert circuit.nb_qudits == 1
+    assert circuit.nb_anyons_per_qudit == 3
     assert circuit.input_charge == 1
     assert circuit.model.name == "Ising"
     assert circuit.dim == 2
