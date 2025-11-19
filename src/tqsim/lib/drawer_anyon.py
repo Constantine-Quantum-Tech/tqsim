97b284410e3aa64b6cc8a1593a630ff623d20aa7
Abduhu <abdellah.a.tounsi@gmail.com>
2025-11-19 15:14:29 +0100

    [ISSUE #63] drawer_anyon

--- src/tqsim/lib/drawer_anyon.py
+++ src/tqsim/lib/drawer_anyon.py
@@ -10,11 +10,13 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+from typing import Any
 import numpy as np
+import numpy.typing as npt
 
 
 class DrawerAnyon:
-    def __init__(self, initial_id, pos):
+    def __init__(self, initial_id: Any, pos: float) -> None:
         self.__i = np.linspace(0, 1)
         self.__one = np.ones_like(self.__i)
         self.__x = [np.copy(self.__i)]
@@ -23,53 +25,53 @@ class DrawerAnyon:
         self.__color = "black"
         self.__label = ""
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__label} (color: {self.__color}"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.__label} (color: {self.__color}"
 
     @property
-    def label(self):
+    def label(self) -> str:
         return self.__label
 
     @label.setter
-    def label(self, new):
+    def label(self, new: str) -> None:
         self.__label = new
 
     @property
-    def color(self):
+    def color(self) -> str:
         return self.__color
 
     @color.setter
-    def color(self, new):
+    def color(self, new: str) -> None:
         self.__color = new
 
     @property
-    def x(self):
+    def x(self) -> list[npt.NDArray[np.float64]]:
         return self.__x
 
     @x.setter
-    def x(self, new):
+    def x(self, new: npt.NDArray[np.float64]) -> None:
         self.__x.append(new)
 
     @property
-    def y(self):
+    def y(self) -> list[npt.NDArray[np.float64]]:
         return self.__y
 
     @y.setter
-    def y(self, new):
+    def y(self, new: npt.NDArray[np.float64]) -> None:
         self.__y.append(new)
 
-    def get_last_x(self):
+    def get_last_x(self) -> np.floating[Any]:
         return self.__x[-1][-1]
 
-    def get_first_y(self):
+    def get_first_y(self) -> np.floating[Any]:
         return self.__y[0][0]
 
-    def get_last_y(self):
+    def get_last_y(self) -> np.floating[Any]:
         return self.__y[-1][-1]
 
-    def add_identity(self):
+    def add_identity(self) -> None:
         self.__x.append(self.__i + self.get_last_x())
         self.__y.append(self.__one * self.get_last_y())
