6e01106eba2dfe1eb5c88ac76d86417c2f7026cb
Abduhu <abdellah.a.tounsi@gmail.com>
2025-11-19 16:41:48 +0100

    [ISSUE #63] test_sanity

--- tests/test_anyon_model_sanity.py
+++ tests/test_anyon_model_sanity.py
@@ -1,4 +1,5 @@
 import itertools
+from typing import Any
 
 import numpy as np
 
@@ -6,23 +7,13 @@ from tqsim.models.fibonacci import FIBONACCI_MODEL
 from tqsim.models.ising import ISING_MODEL
 from tqsim.tools.braiding_generators.fib_multi_qudits import (
     braiding_matrix as fib_b_matrix,
-)
-from tqsim.tools.braiding_generators.fib_multi_qudits import (
     f_matrix as fib_f_matrix,
-)
-from tqsim.tools.braiding_generators.fib_multi_qudits import (
     knitting_matrix as fib_k_matrix,
-)
-from tqsim.tools.braiding_generators.fib_multi_qudits import (
     l_matrix as fib_l_matrix,
 )
 from tqsim.tools.braiding_generators.ising_multi_qudits import (
     braiding_matrix as ising_b_matrix,
-)
-from tqsim.tools.braiding_generators.ising_multi_qudits import (
     knitting_matrix as ising_k_matrix,
-)
-from tqsim.tools.braiding_generators.ising_multi_qudits import (
     l_matrix as ising_l_matrix,
 )
 
@@ -30,7 +21,7 @@ fib_model = FIBONACCI_MODEL
 ising_model = ISING_MODEL
 
 
-def test_f_matrix():
+def test_f_matrix() -> None:
     # iterate over all possible a, b, c, d in [0, 1]
     for a, b, c, d in itertools.product([0, 1], repeat=4):
         expected = fib_model.f_matrix[a, b, c, d, :, :]
@@ -38,7 +29,7 @@ def test_f_matrix():
         assert np.isclose(got, expected, atol=1e-6).all()
 
 
-def test_b_matrix():
+def test_b_matrix() -> None:
     # iterate over all possible a, b, c, d in [0, 1]
     for a, b, c, d in itertools.product([0, 1], repeat=4):
         expected = fib_model.b_matrix[a, b, c, d, :, :]
@@ -46,7 +37,7 @@ def test_b_matrix():
         assert np.isclose(got, expected, atol=1e-6).all()
 
 
-def test_ising_b_matrix():
+def test_ising_b_matrix() -> None:
     # iterate over all possible a, b, c, d in [0, 1]
     for a, b, c, d in itertools.product([0, 1], repeat=4):
         expected = ising_model.b_matrix[a, b, c, d, :, :]
@@ -54,7 +45,7 @@ def test_ising_b_matrix():
         assert np.isclose(got, expected, atol=1e-6).all()
 
 
-def test_l_matrix_fib_and_ising():
+def test_l_matrix_fib_and_ising() -> None:
     # Check L matrix mapping for both Fibonacci and Ising models for q=3
     q = 3
     a = [1 for _ in range(q + 2)]
@@ -84,7 +75,7 @@ def test_l_matrix_fib_and_ising():
         assert np.isclose(l1, l2), f"Ising L mismatch for params: {params}"
 
 
-def test_k_matrix_fib_and_ising():
+def test_k_matrix_fib_and_ising() -> None:
     # Check K (knitting) matrix mapping for both Fibonacci
     # and Ising models for q=3
     q = 2
@@ -93,8 +84,10 @@ def test_k_matrix_fib_and_ising():
     # Fibonacci
     k_matrix = fib_model.compute_knitting_matrix(q)
 
-    def new_k(jm, jmo, jmoo, jmo_, h, i_, i, jj_, jj):
-        return k_matrix[tuple(a + [h, jmoo, jm, jmo, i] + jj + [jmo_, i_] + jj_)]
+    def new_k(
+        jm: int, jmo: int, jmoo: int, jmo_: int, h: int, i_: int, i: int, jj_: list[int], jj: list[int]
+    ) -> Any:
+        return k_matrix[tuple(a + [h, jmoo, jm, jmo, i] + jj + [jmo_, i_] + jj_)]  # type: ignore[call-overload]
 
     for params in itertools.product([0, 1], repeat=3 + 3 + 2 + 2 * q):
         jm, jmo, jmoo, jmo_ = params[0], params[1], params[2], params[3]
@@ -109,8 +102,10 @@ def test_k_matrix_fib_and_ising():
     # Ising
     k_matrix = ising_model.compute_knitting_matrix(q)
 
-    def new_k(jm, jmo, jmoo, jmo_, h, i_, i, jj_, jj):
-        return k_matrix[tuple(a + [h, jmoo, jm, jmo, i] + jj + [jmo_, i_] + jj_)]
+    def new_k_ising(
+        jm: int, jmo: int, jmoo: int, jmo_: int, h: int, i_: int, i: int, jj_: list[int], jj: list[int]
+    ) -> Any:
+        return k_matrix[tuple(a + [h, jmoo, jm, jmo, i] + jj + [jmo_, i_] + jj_)]  # type: ignore[call-overload]
 
     for params in itertools.product([0, 1], repeat=3 + 3 + 2 + 2 * q):
         jm, jmo, jmoo, jmo_ = params[0], params[1], params[2], params[3]
@@ -118,6 +113,6 @@ def test_k_matrix_fib_and_ising():
         jj_ = list(params[7 : 7 + q])
         jj = list(params[7 + q : 7 + 2 * q])
 
-        k1 = new_k(jm, jmo, jmoo, jmo_, h, i_, i, jj_, jj)
+        k1 = new_k_ising(jm, jmo, jmoo, jmo_, h, i_, i, jj_, jj)
         k2 = ising_k_matrix(jm, jmo, jmoo, jmo_, h, i_, i, jj_, jj)
         assert np.isclose(k1, k2), f"Ising K mismatch for params: {params}"
